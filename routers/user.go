2457e8d89c31adc4172ff558197a8e56128db813
zhhnzw <747624368@qq.com>
2021-10-22 21:37:22 +0800

    添加注册路由

--- routers/user.go
+++ routers/user.go
@@ -8,7 +8,7 @@ import (
 func SetUserRouter(router *gin.Engine) {
 	router.POST("/v1/logout", v1.Logout)
 	router.POST("/v1/alterPwd", v1.AlterPwd)
-	userRouter := router.Group("/v1/user")
-	userRouter.GET("", v1.GetUsers)
-	userRouter.POST("", v1.CreateUser)
+	//userRouter := router.Group("/v1/user")
+	//userRouter.GET("", v1.GetUsers)
+	//userRouter.POST("", v1.CreateUser)
 }
