da79975ed25108517ae130e9c8919b98f3824bb5
zhhnzw <747624368@qq.com>
2021-10-27 12:54:22 +0800

    update

--- r/base.r
+++ r/base.r
@@ -1,3 +1,284 @@
+#原函数注释
+#' 使用R基本绘图函数绘制y轴不连续的柱形图
+#'
+#' 绘制y轴不连续的柱形图，具有误差线添加功能。断点位置通过btm和top参数设置，如果不设置，函数可自动计算合适的断点位置。
+#'  gap.barplot function
+#'  df 长格式的data.frame，即数据框中每一列为一组绘图数据。
+#'  y.cols 用做柱形图y值的数据列（序号或名称），一列为一组。
+#'  sd.cols 与y值列顺序对应的误差值的数据列（序号或名称）。
+#'  btm 低位断点。如果btm和top均不设置，程序将自动计算和设置断点位置。
+#'  top 高位断点。
+#'  min.range 自动计算断点的阈值：最大值与最小值的最小比值
+#'  max.fold 自动计算断点时最大值与下方数据最大值的最大倍数比
+#'  ratio 断裂后上部与下部y轴长度的比例。
+#'  gap.width y轴断裂位置的相对物理宽度（非坐标轴实际刻度）
+#'  brk.type 断点类型，可设为normal或zigzag
+#'  brk.bg 断点处的背景颜色
+#'  brk.srt 断点标记线旋转角度
+#'  brk.size 断点标记线的大小（长度）
+#'  brk.col 断点标记线的颜色
+#'  brk.lwd 断点标记线的线宽
+#'  cex.error 误差线相对长度，默认为1
+#'  ... 其他传递给R基本绘图函数barplot的参数
+#'  返回barplot的原始返回值，即柱形图的x坐标
+
+#断轴柱状图--y轴不连续柱状图
+#file_path1     输入文件路径1——数据文件
+#file_path2     输入文件路径2——分组文件
+#out_path       图片输出路径
+#broken_color   柱形图颜色
+#y_cols         用做柱形图y值的数据列（序号或名称），一列为一组
+#sd.cols        与y值列顺序对应的误差值的数据列（序号或名称）
+#btm            下断点值
+#top            上断电值
+#main_title     主标题
+#x_lable        x轴标题
+#y_lable        y轴标题
+#min.range      自动计算断点的阈值：最大值与最小值的最小比值
+#max.fold       自动计算断点时最大值与下方数据最大值的最大倍数比
+#ratio          断裂后上部与下部y轴长度的比例
+#gap.width      y轴断裂位置的相对物理宽度（非坐标轴实际刻度）
+#brk.type       断点类型，可设为normal或zigzag
+#brk.bg         断点处的背景颜色
+#brk.srt        断点标记线旋转角度
+#brk.size       断点标记线的大小（长度）
+#brk.col        断点标记线的颜色
+#brk.lwd        断点标记线的线宽
+#cex.error      误差线相对长度，默认为1
+#...            其他传递给R基本绘图函数barplot的参数
+
+broken_chart <- function(file_path,
+                         out_path,
+                         broken_color = NULL,
+                         btm = NULL,
+                         top = NULL,
+                         main_title = NULL,
+                         x_lable = NULL,
+                         y_lable = NULL,
+                         ratio = 1,
+                         y_cols = NULL,
+                         sd.cols = NULL,
+                         min.range = 10, max.fold = 5,
+                         gap.width = 1, brk.type = "normal",
+                         brk.bg = "white", brk.srt = 135, brk.size = 1,
+                         brk.col = "black", brk.lwd = 1,
+                         cex.error = 1, ...) {
+  library(svglite)
+
+  file_data1 <- read.table(file_path[1], header = TRUE)
+  file_data2 <- read.table(file_path[2], header = FALSE)
+
+  rownum = nrow(file_data1)
+  colnum = ncol(file_data1)
+
+  #数据处理——开始
+  my_data = c()
+
+  for(i in 1:rownum) {
+    for(j in 1:((colnum-1)/3)){
+      my_data = c(my_data, (file_data1[i,(j-1)*3+2]+file_data1[i,(j-1)*3+3]+file_data1[i,(j-1)*3+4])/3)#求取平均值并存储至my_data
+    }
+  }
+
+  df <- array(my_data, dim = c(rownum, (colnum - 1) / 3))#创建二维数组
+
+  #更改数据位置
+  for(i in 1:rownum){
+    for(j in 1:((colnum - 1) / 3)){
+      df[i,j] = my_data[(i-1) * 3 + j]
+    }
+  }
+  #数据处理——结束
+
+  if(is.null(broken_color)){
+    broken_color = rainbow(ncol(df))
+  }
+
+  if(is.null(y_cols)){
+    y.cols = 1:ncol(df)
+  }
+
+  svglite(out_path) #画图开始
+
+  if (missing(df))
+    stop("No data provided.")
+  if (is.numeric(y.cols))
+    ycol <- y.cols else ycol <- colnames(df) == y.cols
+  if (!is.null(sd.cols))
+    if (is.numeric(sd.cols))
+      scol <- sd.cols else scol <- colnames(df) == sd.cols
+  ## Arrange data
+  opts <- options()
+  options(warn = -1)
+  y <- t(df[, ycol])
+  colnames(y) <- NULL
+  if (missing(sd.cols))
+    sdx <- 0 else sdx <- t(df[, scol])
+  sdu <- y + sdx
+  sdd <- y - sdx
+  ylim <- c(0, max(sdu) * 1.05)
+  ## 如果没有设置btm或top，自动计算
+  if (is.null(btm) | is.null(top)) {
+    #自动计算断点位置
+    datax <- sort(as.vector(sdu))
+    flags <- FALSE
+    abtm <- atop <- NULL
+    if (max(datax)/min(datax) < min.range)
+      autox <- list(flag = flags, btm = abtm, top = atop)
+    m <- max(datax)
+    abtm <- datax[2]
+    i <- 3
+    while (m/datax[i] > max.fold) {
+      abtm <- datax[i]
+      flags <- TRUE
+      i <- i + 1
+    }
+    if (flags) {
+      abtm <- abtm + 0.05 * abtm
+      ax <- 2
+      atop <- datax[i] * (ax - 1)/ax
+      while (atop < abtm) {
+        ax <- ax + 1
+        atop <- datax[i] * (ax - 1)/ax
+        if (ax > 100) {
+          flags <- FALSE
+          break
+        }
+      }
+    }
+    autox <- list(flag = flags, btm = abtm, top = atop) #
+    if (autox$flag) {
+      btm <- autox$btm
+      top <- autox$top
+    } else {
+      xx <- barplot(y, beside = TRUE, ylim = ylim, ...)
+      if (!missing(sd.cols)){
+        #绘制误差线
+        horiz = FALSE
+        if (!horiz) {
+          arrows(xx, y, y1 = y - (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+          arrows(xx, y, y1 = y + (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+        } else {
+          arrows(y, xx, x1 = y - (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+          arrows(y, xx, x1 = y + (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+        }
+      }
+      box()
+      return(invisible(xx))
+    }
+  }
+  ## Set up virtual y limits
+  halflen <- btm - ylim[1]
+  xlen <- halflen * 0.1 * gap.width
+  v_tps1 <- btm + xlen  # virtual top positions
+  v_tps2 <- v_tps1 + halflen * ratio
+  v_ylim <- c(ylim[1], v_tps2)
+  r_tps1 <- top  # real top positions
+  r_tps2 <- ylim[2]
+  ## Rescale data
+  lmx <- summary(lm(c(v_tps1, v_tps2) ~ c(r_tps1, r_tps2)))
+  lmx <- lmx$coefficients
+  sel1 <- y > top
+  sel2 <- y >= btm & y <= top
+  y[sel1] <- y[sel1] * lmx[2] + lmx[1]
+  y[sel2] <- btm + xlen/2
+  sel1 <- sdd > top
+  sel2 <- sdd >= btm & sdd <= top
+  sdd[sel1] <- sdd[sel1] * lmx[2] + lmx[1]
+  sdd[sel2] <- btm + xlen/2
+  sel1 <- sdu > top
+  sel2 <- sdu >= btm & sdu <= top
+  sdu[sel1] <- sdu[sel1] * lmx[2] + lmx[1]
+  sdu[sel2] <- btm + xlen/2
+  ## bar plot
+  xx <- barplot(y, beside = TRUE, ylim = v_ylim, axes = FALSE, names.arg = NULL,
+                col = broken_color, main = main_title, xlab = x_lable, ylab = y_lable,
+                ...)
+  ## error bars
+  if (!missing(sd.cols)){
+    #绘制误差线
+    horiz = FALSE
+    if (!horiz) {
+      arrows(xx, y, y1 = y - (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+      arrows(xx, y, y1 = y + (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+    } else {
+      arrows(y, xx, x1 = y - (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+      arrows(y, xx, x1 = y + (sdu - y), length = 0.1 * cex.error, angle = 90, ...)
+    }
+  }
+  ## Real ticks and labels
+  brks1 <- pretty(seq(0, btm, length = 10), n = 4)
+  brks1 <- brks1[brks1 >= 0 & brks1 < btm]
+  brks2 <- pretty(seq(top, r_tps2, length = 10), n = 4)
+  brks2 <- brks2[brks2 > top & brks2 <= r_tps2]
+  labx <- c(brks1, brks2)
+  ## Virtual ticks
+  brks <- c(brks1, brks2 * lmx[2] + lmx[1])
+  axis(2, at = brks, labels = labx)
+  box()
+  ## break marks
+  pos <- par("usr")
+  xyratio <- (pos[2] - pos[1])/(pos[4] - pos[3])
+  xlen <- (pos[2] - pos[1])/50 * brk.size
+  px1 <- pos[1] - xlen
+  px2 <- pos[1] + xlen
+  px3 <- pos[2] - xlen
+  px4 <- pos[2] + xlen
+  py1 <- btm
+  py2 <- v_tps1
+  rect(px1, py1, px4, py2, col = brk.bg, xpd = TRUE, border = brk.bg)
+  x1 <- c(px1, px1, px3, px3)
+  x2 <- c(px2, px2, px4, px4)
+  y1 <- c(py1, py2, py1, py2)
+  y2 <- c(py1, py2, py1, py2)
+  xx1 <- x1 - xlen * cos(brk.srt * pi/90)
+  yy1 <- y1 + xlen * sin(brk.srt * pi/90)/xyratio
+  xx2 <- x2 + xlen * cos(brk.srt * pi/90)
+  yy2 <- y2 - xlen * sin(brk.srt * pi/90)/xyratio
+  px <- list(x1 = xx1, x2 = xx2, y1 = yy1, y2 = yy2)
+  if (brk.type == "zigzag") {
+    x1 <- c(x1, px1, px3)
+    x2 <- c(x2, px2, px4)
+    if (brk.srt > 90) {
+      y1 <- c(y1, py2, py2)
+      y2 <- c(y2, py1, py1)
+    } else {
+      y1 <- c(y1, py1, py1)
+      y2 <- c(y2, py2, py2)
+    }
+  }
+  if (brk.type == "zigzag") {
+    px$x1 <- c(pos[1], px2, px1, pos[2], px4, px3)
+    px$x2 <- c(px2, px1, pos[1], px4, px3, pos[2])
+    mm <- (v_tps1 - btm)/3
+    px$y1 <- rep(c(v_tps1, v_tps1 - mm, v_tps1 - 2 * mm), 2)
+    px$y2 <- rep(c(v_tps1 - mm, v_tps1 - 2 * mm, btm), 2)
+  }
+  par(xpd = TRUE)
+  segments(px$x1, px$y1, px$x2, px$y2, lty = 1, col = brk.col, lwd = brk.lwd)
+  options(opts)
+  par(xpd = FALSE)
+  invisible(xx)
+
+
+  graphics.off() #画图结束
+}
+
+
+# #使用示例
+# broken_chart("C:/Users/w1792/Desktop/test/broken_columns/broken_columns_genus.txt",
+#              "C:/Users/w1792/Desktop/test/broken_columns/broken_columns_group.txt",
+#              "C:/Users/w1792/Desktop/broken.svg",
+#              btm = 1,
+#              top = 3,
+#              main_title = "Broken Columns",
+#              x_lable = "Group",
+#              y_lable = "Data"
+# )
+#
+
+
+
 # 3. beeswarm_chart       蜂群图
 beeswarm_chart = function(file_path, out_path, main_name = "", xlab_name = "", ylab_name = "", bee_color){
   library(reshape2)
@@ -40,6 +321,103 @@ beeswarm_chart = function(file_path, out_path, main_name = "", xlab_name = "", y
 
 }
 # beeswarm_chart('E:/eclipse_workspace/biologicalCloudTools/public/UploadFolder/001@qq.com/20210503003846_fengqun.txt','E:/eclipse_workspace/biologicalCloudTools/public/UserFolder/001@qq.com/20210503003846_fengqun.svg','蜂群图','X轴','Y轴',c("#FF0000","#00FF95","#3C00FF"))
+
+#file_path          输入文件路径
+#out_path           输出图片路径
+#p_width            图片宽度——inches
+#p_height           图片高度——inches
+#set_lable          集合（横矩形）名
+#y_lable            纵矩形名
+#point_color        点颜色
+#y_color            纵矩形颜色
+#set_color          集合(横矩形)颜色
+#ydata_show         y轴数据是否显示——"yes" or "no"
+#order_by           矩阵中交集排序方式，"freq"从大到小排序，其他选项degree, 先freq后degree
+#point_size         点大小
+#line_size          线宽度
+#upset_order        按使用sets参数输入的顺序保留集合。函数中默认值为FALSE，按大小对集合进行排序，此处为TRUE
+#intersection_num   要绘制的交点数。如果设置为NA，将绘制所有交点
+
+upset_chart <- function(file_path,
+                        out_path,
+                        p_width = 8,
+                        p_height = 8,
+                        set_lable = "Set Size",
+                        y_lable = "Intersection Size",
+                        point_color = c("black"),
+                        y_color = c("black"),
+                        set_color = NULL,
+                        ydata_show = "yes",
+                        order_by = c("freq"),
+                        point_size = 3,
+                        line_size = 1,
+                        upset_order = TRUE,
+                        intersection_num = NA
+){
+  library(UpSetR)
+  library(svglite)
+
+  file_data <- read.table(file_path, header = TRUE, sep = "\t")
+
+  col_name <- colnames(file_data)
+
+  colnum = ncol(file_data)
+  rownum = nrow(file_data)
+
+  datalist <- list()
+
+  for(i in 1:colnum){
+    t = c()
+    for(j in 1:rownum){
+      if(is.na(file_data[j,i])){
+        break
+      }
+      t = c(t,file_data[j,i])
+    }
+
+    datalist_t = list(t)
+    datalist = c(datalist, datalist_t)
+  }
+
+  names(datalist) <- col_name #修改列表名
+
+  if(is.null(set_color)){
+    set_color = rainbow(colnum) #设置集合默认颜色
+  }
+
+  p <- upset(fromList(datalist),
+             nsets = length(datalist),
+             sets = col_name,
+             order.by = order_by, #矩阵中交集排序方式，"freq"从大到小排序，其他选项degree, 先freq后degree
+             point.size = point_size, #点大小
+             line.size = line_size, #线宽度
+             text.scale = c(1.5, 1.2, 1.2, 1, 1.5, 1), # ytitle, ylabel, xtitle, xlabel, sets, number
+             matrix.color = point_color, #点颜色
+             main.bar.color = y_color, #y轴颜色
+             mainbar.y.label = y_lable, #y轴标题
+             sets.bar.color = set_color, #x轴集合颜色
+             nintersects = intersection_num, #要绘制的交点数。如果设置为NA，将绘制所有交点
+             keep.order = upset_order, #按使用sets参数输入的顺序保留集合。默认值为FALSE，按大小对集合进行排序
+             sets.x.label = set_lable, #x轴标题
+             show.numbers = ydata_show, #y轴数据是否显示
+             group.by = "degree" #数据分组依据("degree" or "sets")
+  )
+
+  #保存图片
+  svglite(out_path, width = p_width, height = p_height)
+  print(p) #该图片只能通过print方式写至创建的空白画布中
+  graphics.off() #upset画图开启的好像不是一条线，所以需要使用graphics.off()函数，全部关闭
+}
+
+
+# upset_chart("seniorvenn.txt", #输入文件路径
+#             "C:/Users/w1792/Desktop/upsetchart.svg", #图片输出路径
+#             8,8, #宽✖高
+#             "Set Size", #集合(横矩形)名
+#             "Intersection Size", #纵矩形名
+#             order_by = c("freq") #c("freq"), c("degree"), c("freq", "degree")
+# )
+
 
 # 5. podfigure            豆荚图
 # podfigure(c('E:/eclipse_workspace/biologicalCloudTools/public/UploadFolder/001@qq.com/20210503160531_doujia1.txt',
@@ -247,6 +625,7 @@ ternaryplot_plot <- function(file_path, out_path, main_name = "",points_size = N
 
 
 # 9. circos     circos物种关系图
+# circos(c('E:/idea_java_code/biologicalCloudToolsData/UploadFolder/001@qq.com/20210630192914_taxonomy.txt','E:/idea_java_code/biologicalCloudToolsData/UploadFolder/001@qq.com/20210630192914_group.txt','E:/idea_java_code/biologicalCloudToolsData/UploadFolder/001@qq.com/20210630192914_otu_table.txt'),'E:/idea_java_code/biologicalCloudToolsData/UserFolder/001@qq.com/20210630192914_taxonomy.pdf')
 circos =function(data_filepath,out_picpath,width_n = 20, height_n = 8){
   library(circlize)
   library(dplyr)
@@ -555,9 +934,77 @@ pie =function(data_filepath,out_picpath,title_n,theme_n=NULL,type_n=2,sub_title=
 
 }
 # pie('E:/eclipse_workspace/biologicalCloudTools/public/UploadFolder/001@qq.com/20210506220332_pie.txt','E:/eclipse_workspace/biologicalCloudTools/public/UserFolder/001@qq.com/20210506220332_pie.html','pie','shine',1,NULL)
+# pie('E:/idea_java_code/biologicalCloudToolsData/UploadFolder/001@qq.com/20210701170457_pie.txt','E:/idea_java_code/biologicalCloudToolsData/UserFolder/001@qq.com/20210701170457_pie.html','pie','shine',1,'p1')
 
 
 # 15. scatter3d         3D散点图
+#x-axis:选择作为x轴的列数
+#y-axis:选择作为Y轴的列数
+#p_col:点的颜色
+#p_size:点的大小
+#title_name:标题名字
+#h_val:做y=h_val的水平辅助线
+#h_col:水平辅助线的颜色
+#h_lty:水平辅助线的样式（其中1为实线，2为虚线，3为点线，4为点线线段）
+#v_val:做x=v_val的竖直辅助线
+#v_col:竖直辅助线的颜色
+#v_lty:同h_lty
+#d_val:斜辅助线y=kx+b中b的值
+#d_slo:斜辅助线y=kx+b中k的值
+#d_lty:斜辅助线的样式
+#d_col:斜辅助线的颜色
+
+#读入为txt文件，三列分别为x,y,z，有表头
+#size_n=.1为点的大小
+#shape_n为点的形状:p为点图、s为球面图、l为线图、h为透视线图；
+#color_scatter为点的颜色'royalblue1', 'darkcyan', 'oldlace','cornsilk','coral',
+#'chartreuse','crimson','blue','black','magenta','yellow')
+#输出格式为html，输出大小width_n × height_n
+#实例scatter3d('scatter3d.txt','scatter.html',X_name = 'x',Y_name = 'y',Z_name = 'z')
+
+# scatter3d =function(data_filepath,out_picpath,size_n=.2,shape_n='s',color_scatter="red",X_name=NULL,Y_name=NULL,Z_name=NULL,width_n=600,height_n=600){
+#   library(rgl)
+#   library(magick)
+#   data1<-read.table(data_filepath, header = T)#输入是表格
+#   x=c(data1[,1])
+#   y=c(data1[,2])
+#   z=c(data1[,3])
+#   plot3d( x, y, z, type = shape_n, col=color_scatter,radius =size_n ,
+#           xlab=X_name, ylab=Y_name, zlab=Z_name)
+#   out<-writeWebGL(filename=out_picpath,width=width_n,height=height_n)#保存为html
+#   dev.off()
+#   return(out)
+# }
+
+
+# scatter_plot("andy","/Users/nieandi/Desktop/scatter/scatter_exa.txt","/Users/nieandi/Desktop/scatter/scatter_exa.jpg",
+#              2,3,"red",1,
+#              "scatter plot",5000,2,"black",5000,2,"black",5000,2,3,"black")
+
+scatter_plot<-function(file_path,out_path,
+                       x_axis=2,y_axis=3,p_col="red",p_size=1,title_name="scatter plot",
+                       h_val=5000,h_lty=2,h_col="black",v_val=5000,v_lty=2,v_col="black",
+                       d_val=5000,d_slo=2,d_lty=3,d_col="black"){
+  library(ggplot2)
+  data_F<-read.table(file_path,header = TRUE,sep="\t",na.strings=c("NA"))
+  data<-read.table(file_path,header = TRUE,sep="\t",na.strings=c("NA"))
+  #head(data)
+  row<-dim(data)[2]
+  print(row)
+
+  p<-ggplot(data,aes(x=data[,x_axis],y=data[,y_axis]))+
+    geom_point(color=p_col,size=p_size)+
+    labs(title=title_name,x=data_F[1,x_axis],y=data_F[1,y_axis])+
+    geom_hline(yintercept = h_val,lty=h_lty,color=h_col)+
+    geom_vline(xintercept = v_val,lty=v_lty,color=v_col)+
+    geom_abline(intercept=d_val,slope=d_slo,lty=d_lty,color=d_col)
+  theme_bw()
+  p
+  ggsave(out_path)
+}
+
+
+
 
 # 16 线性回归图
 regressionline =function(data_filepath,out_picpath,title_n=NULL,size_n=2,color_scatter="black",color_line="red",X_name=NULL,Y_name=NULL,shape_n=16,type_n=2){
@@ -860,6 +1307,37 @@ vn_plot<-function(filename,save_path){
 
 
 # 22. krona        Krona层级饼图
+# sunburst_plot("andy","/Users/nieandi/Desktop/sun/sun.txt","/Users/nieandi/Desktop/sun/ex.html")
+# sunburst_plot<-function(file_path,save_path){
+#   library(sunburstR)
+#   library(R2HTML)
+#   data<-read.table(file_path,header=TRUE,sep="\t",na.strings=c("NA"))
+#
+#   head(data)
+#   m<-dim(data)[2]#列数
+#   n<-dim(data)[1]#行数
+#   for(i in 1:n){
+#     for(j in 1:m){
+#       if(data[i,j]==""){
+#         data[i,j]=NA
+#       }
+#     }
+#   }
+#   data$val<-data[,2]
+#   if(m>2){
+#     for(i in 3:m){
+#       data$val=paste(data$val,data[,i],sep="-")
+#     }
+#   }
+#   newval<-gsub("-NA","" ,data$val)#将数据中的空值剔除
+#   newval<-as.data.frame(newval)
+#   num<-data[,1]
+#   num<-as.data.frame(num)
+#   d<-cbind(newval,num)
+#   sunburst(d)
+#   sunhtml<-sunburst(d)
+#   HTML(save_path,sunhtml)
+# }
 
 # 23. maplot                MA图
 # MA图
@@ -872,9 +1350,12 @@ vn_plot<-function(filename,save_path){
 #down_col:down点的颜色
 #no_col：no点的颜色
 #up_col：up点的颜色
-maplot=function(file_name,out_path,title_name,a_column,b_column,q_column,
-                 p_size,x_name,y_name,
-                 down_col,no_col,up_col){
+# maplot("andy","/Users/nieandi/Desktop/MA/MA_exa.txt","/Users/nieandi/Desktop/MA/MA_exa.jpg",
+#        "MA_plot",4,5,7,1,
+#         "A","M","red","gray","green")
+maplot=function(file_name,out_path,title_name="MA_plot",a_column=4,b_column=5,q_column=7,
+                 p_size=1,x_name="A",y_name="M",
+                 down_col="red",no_col="gray",up_col="green"){
   library(ggplot2)
   data<-read.table(file_name,header = TRUE,sep="\t",na.strings=c("NA"))
   head(data)
@@ -896,8 +1377,43 @@ maplot=function(file_name,out_path,title_name,a_column,b_column,q_column,
   p
   ggsave(out_path)
 }
-# maplot("andy","/Users/nieandi/Desktop/MA/MA_exa.txt","/Users/nieandi/Desktop/MA/MA_exa.jpg","MA_plot",4,5,7,1,
-#         "A","M","red","gray","green")
+
+
+#24.火山图
+#例：volcano_plot("andy","/Users/nieandi/Desktop/volcano/vol.csv","/Users/nieandi/Desktop/volcano/1.jpg","vol",1.5,4,30,6,2,7,0.01,"#2f5688","#BBBBBB","#CC0000")
+#x_column:log2(FC)所在的列数，x轴所代表的值
+#y_column:P.value或者Q.value所在的列数，y轴所代表的值
+#x_val:x轴阙值，差异倍数的阙值，通常为1.5或2（可以自行输入其他数字）
+#y_val:y轴阙值，P.value或者Q.value的阙值，通常为0.01和0.05（二选一就可）
+#x_max:x轴的上界
+#y_max:y轴的上界
+volcano_plot<-function(file_name,out_path,title_name,p_size,x_max,y_max,x_column,x_val,y_column,y_val,
+                       down_col,not_col,up_col){
+  library(ggplot2)
+  library(ggrepel)
+  df<-read.table(file_name,header = T,
+                 sep="\t",na.strings=c("NA"))
+  #head(df)
+  #dim(df)
+  df$group<- ifelse(df[,x_column]>=x_val&df[,y_column]<=y_val,"Up",
+                    ifelse(df[,x_column]<=(-1)*x_val&df[,y_column]<=y_val,"Down","Not sig"))
+  table(df$group)#算分组后的个数
+  #df$pvalue_log10<-(-log10(PValue))
+  #df1<-df[df$pvalue_log10>=10,]#把值挑出来
+  #dim(df1)
+  ggplot(df,aes(x=df[,x_column],y=-log10(df[,y_column])))+
+    geom_point(aes(color=group),size=p_size)+
+    xlim((-1)*x_max,x_max)+
+    ylim(0,y_max)+
+    scale_color_manual(values=c(down_col,not_col,up_col))+#c("#2f5688","#BBBBBB","#CC0000")
+    ##geom_label_repel(data=df1,aes(x=logFC,y=-log10(P.Value),label=gene_name))+
+    labs(title=title_name,x ="log2(FC)", y ="-log10(Pvalue)")+
+    geom_hline(yintercept = 1.30,linetype="dashed")+
+    geom_vline(xintercept = c((-1)*x_val,x_val),linetype="dashed")+#xy虚线
+    theme_bw()
+  ggsave(out_path)
+}
+# volcano_plot("andy","/Users/nieandi/Desktop/volcano/vol_exa.txt","/Users/nieandi/Desktop/volcano/vol_exa.jpg","vol",1.5,4,30,6,2,7,0.01,"#2f5688","#BBBBBB","#CC0000")
 
 
 
--- r/main.r
+++ r/main.r
@@ -1,4 +1,6 @@
 source('./base.r')
+source('./FunctionalAnalysis.r')
+source('./ClusteringAnalysis.r')
 # pie('./pie.txt','./pie1.html','pie',NULL,2,NULL)
 
 # beeswarm_chart('./sample/BaseFunction/beeswarm_chart/beeswarm_exam.txt','./beeswarm.svg','蜂群图','X轴','Y轴',c("#FF0000","#00FF95","#3C00FF"))
@@ -27,3 +29,33 @@ source('./base.r')
 # FALSE,'groupedviolin')
 
 # violin('./sample/BaseFunction/violin/violin.txt','./violin.png',FALSE)
+
+# data_path=c("./sample/BaseFunction/box_line/box.txt")
+# save_path="./box_out.svg"
+# groupname=c("group1")
+# box_line(data_path,save_path,groupname,c(),NULL,NULL)
+
+# volcano_plot("./sample/BaseFunction/volcano_plot/vol_exa.txt","./vol_exa.svg","vol",1.5,4,30,6,2,7,0.01,"#2f5688","#BBBBBB","#CC0000")
+
+# maplot("./sample/BaseFunction/maplot/MA_exa.txt","./MA_exa.jpg",
+#        "MA_plot",4,5,7,1,
+#         "A","M","red","gray","green")
+
+# upset_chart("./sample/BaseFunction/upset_chart/seniorvenn.txt", #输入文件路径
+#             "./upsetchart.svg", #图片输出路径
+#             8,8, #宽✖高
+#             "Set Size", #集合(横矩形)名
+#             "Intersection Size", #纵矩形名
+#             order_by = c("freq") #c("freq"), c("degree"), c("freq", "degree")
+# )
+
+
+# FunctionalAnalysis.r
+# COG("./sample/FunctionalAnalysis/COG/cog.class.annot.txt","./COG.svg",1)
+
+# ClusteringAnalysis.r
+# NMDS(c('./sample/ClusteringAnalysis/NMDS/nmds_out.txt','./sample/ClusteringAnalysis/NMDS/nmds_group.txt'),'./nmds.svg',18,18)
+PCOA(c('./sample/ClusteringAnalysis/PCOA/pcoa_out.txt','./sample/ClusteringAnalysis/PCOA/pcoa_group.txt'),'pcoa.svg')
+# tsne_chart("./sample/ClusteringAnalysis/tsne_chart/tsne.txt",
+#            "./tsne.svg",
+#            "./tsne.txt")
